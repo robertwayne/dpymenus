589a6e62804dadfedaab439334d46671bbbbcc87
Rob <13954303+robertwayne@users.noreply.github.com>
2020-04-02 23:03:02 -0400

    Changed discord repo name to match the project name.

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open('README.md', 'r') as file:
 
 setuptools.setup(
         name='dpymenus',
-        version='1.0.0',
+        version='1.0.1',
         author='Rob Wagner',
         author_email='rob.wagner@outlook.com',
         description='Dynamic, composable dpymenus for use with the discord.py library.',
         long_description=long_description,
         long_description_content_type='text/markdown',
-        url='https://github.com/robertwayne/discord-menus',
+        url='https://github.com/robertwayne/dpymenus',
         packages=setuptools.find_packages(),
         classifiers=[
             'Programming Language :: Python :: 3.8',

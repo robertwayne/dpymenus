309093f03b00404b40878b3b3643c3797705f01d
Rob <13954303+robertwayne@users.noreply.github.com>
2020-05-07 20:26:29 -0400

    EXPERIMENTAL: Polls work somewhat. Need a lot of cleanup + optimization.

--- dpymenus/poll.py
+++ dpymenus/poll.py
@@ -1,17 +1,11 @@
-import discord
-from discord.abc import GuildChannel
-
-from dpymenus.base_menu import BaseMenu
 import asyncio
-from typing import Dict, List, Optional, Set
+from typing import Set
 
-from discord import Emoji, Message, PartialEmoji
-from discord.abc import GuildChannel
+from discord import User
 from discord.ext.commands import Context
 
-from dpymenus.base_menu import BaseMenu
-from dpymenus.button_menu import ButtonMenu
-from dpymenus.page import Page
+from dpymenus import ButtonMenu
+from dpymenus.exceptions import NoButtonsError
 
 
 class Poll(ButtonMenu):
@@ -22,15 +16,14 @@ class Poll(ButtonMenu):
 
     Attributes:
         ctx: A reference to the command Context.
-        pages: A list containing references to Page objects.
         timeout: How long (in seconds) before the poll ends.
         voted: Dictionary that tracks users who have already voted.
     """
 
-    def __init__(self, ctx: Context, pages: List[Page], timeout: int = 300):
-        super().__init__(ctx, pages, timeout)
-        self.voted: Set = set()
-        await self._set_state_fields()
+    def __init__(self, ctx: Context, timeout: int = 300):
+        super().__init__(ctx, timeout)
+        self.voted: Set[User] = set()
+        self.cheaters: int = 0
 
     def __repr__(self):
         return f'<Menu pages={[p.__str__() for p in self.pages]}, timeout={self.timeout}, active={self.active} page={self.page},' \
@@ -39,28 +32,49 @@ class Poll(ButtonMenu):
     async def run(self):
         """The entry point to a new Menu instance. This will start a loop until a Page object with None as its function is set.
         Manages gathering user input, basic validation, sending messages, and cancellation requests."""
+        self._set_state_fields()
+
         self.output = await self.ctx.send(embed=self.pages[self.page])
         await self.add_buttons()
 
         while self.active:
-            _loop = asyncio.get_event_loop()
-            _loop.create_task(self.process_vote())
+            await asyncio.sleep(self.timeout)
+            msg = await self.ctx.channel.fetch_message(self.output.id)
+            for reaction in msg.reactions:
+                if reaction.me is True:
+                    users = set(await reaction.users().flatten())
+                    users.remove(self.ctx.bot.user)
+                    self.state_fields[reaction.emoji] = await self.valid_votes(users)
+
+            for k, v in self.state_fields.items():
+                self.state_fields[k] = max(0, v - self.cheaters)
+
+            await self.pages[self.page].callback(self)
+
+    async def valid_votes(self, users: Set[User]) -> int:
+        count = 0
+        for user in users:
+            if user not in self.voted:
+                count += 1
+                self.voted.add(user)
+            else:
+                self.cheaters += 1
+        return count
 
     async def validate_vote(self):
         return True if self.ctx.author not in self.voted else False
 
-    async def _set_state_fields(self):
+    def _set_state_fields(self):
+        self._validate_buttons()
+
         for button in self.pages[self.page].buttons:
             self.state_fields.update({button: 0})
-        print(self.state_fields)
 
-    async def process_vote(self):
-        try:
-            await self.ctx.bot.wait_for('reaction_add', timeout=self.timeout, check=lambda u: u not in self.voted)
-        except asyncio.TimeoutError:
-            await self._timeout()
-        else:
-            for k, v in self.state_fields.items():
-                print('adding on button press')
-                if k == self.input.content:
-                    k[v] += 1
+    def _validate_buttons(self):
+        for page in self.pages:
+            if page.callback is None:
+                return
+
+            if len(page.buttons) <= 1:
+                raise NoButtonsError('A Poll primary page must have at least two buttons.')
+

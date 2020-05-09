9b86abd4f41e57762b65175da23b0ba2f497fe6a
Rob <13954303+robertwayne@users.noreply.github.com>
2020-05-09 17:37:16 -0400

    Added four new callback event overrides on pages.

--- dpymenus/page.py
+++ dpymenus/page.py
@@ -7,12 +7,17 @@ class Page(Embed):
     """Represents a single page inside a menu. Subclasses the discord.py Embed class.
 
     Attributes:
-        callback: Reference to a Callable.
-        buttons: A list of button objects.
+        on_next: Reference to a Callable.
+        buttons: A list of reaction Emoji objects or unicode strings.
     """
 
-    def __init__(self, callback: Optional[Callable] = None, buttons: List = None, **kwargs):
-        self.callback = callback
+    def __init__(self, on_next: Optional[Callable] = None, on_fail: Optional[Callable] = None,
+                 on_cancel: Optional[Callable] = None, on_timeout: Optional[Callable] = None,
+                 buttons: List = None, **kwargs):
+        self.on_next = on_next
+        self.on_fail = on_fail
+        self.on_cancel = on_cancel
+        self.on_timeout = on_timeout
         self.buttons = buttons
         super().__init__(**kwargs)
 
@@ -20,12 +25,4 @@ class Page(Embed):
         return f'<Page {self.title}>'
 
     def __repr__(self):
-        return f'Page(title={self.title} callback={self.callback})'
-
-
-class DynamicPage(Page):
-    pass
-
-
-class StaticPage(Page):
-    pass
+        return f'Page(title={self.title} on_next={self.on_next})'

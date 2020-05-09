2803a542a892f63a29b239a08b1b20fb322e4910
Rob <13954303+robertwayne@users.noreply.github.com>
2020-05-09 19:20:06 -0400

    Updated Page __repr__ and cleaned up signature.

--- dpymenus/page.py
+++ dpymenus/page.py
@@ -11,18 +11,18 @@ class Page(Embed):
         buttons: A list of reaction Emoji objects or unicode strings.
     """
 
-    def __init__(self, on_next: Optional[Callable] = None, on_fail: Optional[Callable] = None,
-                 on_cancel: Optional[Callable] = None, on_timeout: Optional[Callable] = None,
-                 buttons: List = None, **kwargs):
+    def __init__(self, buttons: Optional[List] = None, on_next: Optional[Callable] = None, on_fail: Optional[Callable] = None,
+                 on_cancel: Optional[Callable] = None, on_timeout: Optional[Callable] = None, **kwargs):
+        self.buttons = buttons
         self.on_next = on_next
         self.on_fail = on_fail
         self.on_cancel = on_cancel
         self.on_timeout = on_timeout
-        self.buttons = buttons
         super().__init__(**kwargs)
 
     def __str__(self):
         return f'<Page {self.title}>'
 
     def __repr__(self):
-        return f'Page(title={self.title} on_next={self.on_next})'
+        return f'Page(title={self.title} on_next={self.on_next}, on_fail={self.on_fail}, on_cancel={self.on_cancel}, on_timeout={self.on_timeout}, ' \
+               f'buttons={self.buttons}, {super().__repr__()})'

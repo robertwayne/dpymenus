f2b57453999f9fe9062341a785cfb3882f51aa66
Rob <13954303+robertwayne@users.noreply.github.com>
2020-05-09 17:38:18 -0400

    Version bump.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as file:
 
 setuptools.setup(
         name='dpymenus',
-        version='0.2.0',
+        version='0.2.1',
         author='Rob Wagner',
         author_email='rob.wagner@outlook.com',
         description='Dynamic, composable menus and polls for use with the discord.py library.',

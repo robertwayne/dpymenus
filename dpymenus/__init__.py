9ffcf64f3da9f0aa449b3fc33d1079ebc8350911
Rob <13954303+robertwayne@users.noreply.github.com>
2020-05-08 21:58:35 -0400

    Organized imports.

--- dpymenus/__init__.py
+++ dpymenus/__init__.py
@@ -1,5 +1,5 @@
 from dpymenus.base_menu import BaseMenu
-from dpymenus.text_menu import TextMenu
 from dpymenus.button_menu import ButtonMenu
-from dpymenus.poll import Poll
 from dpymenus.page import Page
+from dpymenus.poll import Poll
+from dpymenus.text_menu import TextMenu

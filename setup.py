ed447ee3548aece6be21292a7843f842141a7f47
Rob <13954303+robertwayne@users.noreply.github.com>
2020-07-17 16:29:01 -0400

    Version bump for PyPi.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as file:
 
 setuptools.setup(
         name='dpymenus',
-        version='0.2.9',
+        version='0.2.10',
         author='Rob Wagner',
         author_email='rob.wagner@outlook.com',
         description='Dynamic, composable menus and polls for use with the discord.py library.',

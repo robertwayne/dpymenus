c742418ff6a625111fe346494d97d980a4bbb8fe
Rob <13954303+robertwayne@users.noreply.github.com>
2020-08-11 18:34:09 -0400

    0.3.5a5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as file:
 
 setuptools.setup(
         name='dpymenus',
-        version='0.3.5a4',
+        version='0.3.5a5',
         author='Rob Wagner',
         author_email='rob.wagner@outlook.com',
         license='License :: OSI Approved :: MIT License',

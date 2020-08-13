2adc85c0cf7bacfc310442eec85c6a977b38e29c
Rob <13954303+robertwayne@users.noreply.github.com>
2020-08-12 22:57:58 -0400

    Version bump to 0.3.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as file:
 
 setuptools.setup(
         name='dpymenus',
-        version='0.3.5b',
+        version='0.3.5',
         author='Rob Wagner',
         author_email='rob.wagner@outlook.com',
         license='License :: OSI Approved :: MIT License',

8a2ec8fd783abc705ddb85036210f53fdbd3c75f
Rob <13954303+robertwayne@users.noreply.github.com>
2020-08-17 19:01:39 -0400

    Fixing __slots__ usage.

--- dpymenus/page.py
+++ dpymenus/page.py
@@ -12,16 +12,16 @@ class Page:
     Attributes
         :embed: A discord Embed object. Used in place of utilizing the Page as an Embed object itself.
     """
-    __slots__ = ('index', 'embed', 'buttons', 'on_next', 'on_fail', 'on_cancel', 'on_timeout')
+    __slots__ = ('index', 'embed', '_buttons', '_on_next', '_on_fail', '_on_cancel', '_on_timeout')
 
     def __init__(self, embed: Embed):
         self.index: int = 0
         self.embed: Embed = embed
-        self.buttons: ButtonList = []
-        self.on_next: Callback = None
-        self.on_fail: Callback = None
-        self.on_cancel: Callback = None
-        self.on_timeout: Callback = None
+        self._buttons: ButtonList = []
+        self._on_next: Callback = None
+        self._on_fail: Callback = None
+        self._on_cancel: Callback = None
+        self._on_timeout: Callback = None
 
     def __repr__(self):
         return f"Page(title={self.embed.title} {''.join([f'{k}={v}' for k, v in self.__dict__.items()])})"
@@ -31,30 +31,30 @@ class Page:
 
     def set_buttons(self, buttons: List) -> 'Page':
         """Generates reaction buttons when the page is displayed. Returns itself for fluent-style chaining."""
-        self.buttons = buttons
+        self._buttons = buttons
 
         return self
 
     def on_next(self, func: Callable) -> 'Page':
         """Sets the function that will be called when the `next` event runs. Returns itself for fluent-style chaining."""
-        self.on_next = func
+        self._on_next = func
 
         return self
 
     def on_fail(self, func: Callable) -> 'Page':
         """Sets the function that will be called when the `fail` event runs. Returns itself for fluent-style chaining."""
-        self.on_fail = func
+        self._on_fail = func
 
         return self
 
     def on_cancel(self, func: Callable) -> 'Page':
         """Sets the function that will be called when the `cancel` event runs. Returns itself for fluent-style chaining."""
-        self.on_cancel = func
+        self._on_cancel = func
 
         return self
 
     def on_timeout(self, func: Callable) -> 'Page':
         """Sets the function that will be called when the `timeout` event runs. Returns itself for fluent-style chaining."""
-        self.on_timeout = func
+        self._on_timeout = func
 
         return self

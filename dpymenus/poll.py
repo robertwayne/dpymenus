bafd35e049653403938c4edbbe424ac463c2f523
Rob Wagner <13954303+robertwayne@users.noreply.github.com>
2020-08-23 22:02:31 -0400

    clean up poll code

--- dpymenus/poll.py
+++ dpymenus/poll.py
@@ -32,8 +32,13 @@ class Poll(ButtonMenu):
         pending = set()
         while self.active:
             try:
-                _, pending = await asyncio.wait([asyncio.create_task(self._get_vote_add()), asyncio.create_task(self._get_vote_remove()),
-                                                 asyncio.create_task(self._poll_timer())], return_when=asyncio.FIRST_COMPLETED)
+                _, pending = await asyncio.wait(
+                        [
+                            asyncio.create_task(self._get_vote_add()),
+                            asyncio.create_task(self._get_vote_remove()),
+                            asyncio.create_task(self._poll_timer())
+                         ],
+                        return_when=asyncio.FIRST_COMPLETED)
             finally:
                 for task in pending:
                     task.cancel()

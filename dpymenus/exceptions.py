62ec02af11bc448f4029ab768790b61a6a780288
Rob Wagner <13954303+robertwayne@users.noreply.github.com>
2020-09-10 10:26:41 -0400

    error handler work

--- dpymenus/exceptions.py
+++ dpymenus/exceptions.py
@@ -1,19 +1,19 @@
-class MenuError(Exception):
-    def __init__(self, message):
+class MenuException(Exception):
+    def __init__(self, message: str = None):
         self.message = message
 
 
-class PagesError(MenuError):
+class PagesError(MenuException):
     pass
 
 
-class ButtonsError(MenuError):
+class ButtonsError(MenuException):
     pass
 
 
-class EventError(MenuError):
+class EventError(MenuException):
     pass
 
 
-class SessionError(MenuError):
+class SessionError(MenuException):
     pass

203b44ea86fe94f7925758133c076af6ec70bab6
Rob Wagner <13954303+robertwayne@users.noreply.github.com>
2020-09-29 10:59:21 -0400

    Drop support for discord.py < 1.5
    Mandatory update as of Oct 7th.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setuptools.setup(
         ],
         python_requires='>=3.7',
         install_requires=[
-            'discord.py>=1.4.1',
+            'discord.py>=1.5',
             'emoji==0.6.0'
         ]
 )

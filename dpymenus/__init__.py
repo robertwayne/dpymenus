f253f4315777a6500d17034ee44f561b6ea7c592
Rob <rob@robwagner.dev>
2021-01-14 00:17:43 -0500

    Moved menu classes into 'menus' dir.

--- dpymenus/__init__.py
+++ dpymenus/__init__.py
@@ -8,8 +8,8 @@ logger = logging.getLogger("dpymenus")
 logger.addHandler(logging.NullHandler())
 
 from dpymenus.page import Page
-from dpymenus.base_menu import BaseMenu
-from dpymenus.text_menu import TextMenu
-from dpymenus.button_menu import ButtonMenu
-from dpymenus.paginated_menu import PaginatedMenu
-from dpymenus.poll import Poll
+from dpymenus.menus.base_menu import BaseMenu
+from dpymenus.menus.text_menu import TextMenu
+from dpymenus.menus.button_menu import ButtonMenu
+from dpymenus.menus.paginated_menu import PaginatedMenu
+from dpymenus.menus.poll import Poll

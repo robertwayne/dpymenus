a7daa41433eb9eb4df2c79fe8a9e873784d197ae
Rob <rob@robwagner.dev>
2021-01-31 01:36:07 -0500

    Session creation now handles the session store validation.

--- dpymenus/sessions/session.py
+++ dpymenus/sessions/session.py
@@ -2,31 +2,50 @@ from typing import Optional, TYPE_CHECKING
 
 from discord.ext.commands import Context
 
-from dpymenus import sessions
+from dpymenus import SessionError, sessions
+from dpymenus.settings import PREVENT_MULTISESSIONS
 
 if TYPE_CHECKING:
-    from dpymenus.types import Menu
+    from dpymenus.types import Menu, SessionKey
 
 
 class Session:
-    def __init__(self, instance: "Menu"):
-        self.instance = instance
-        self.owner = instance.ctx.author.id
-
-        sessions.update({(self.owner, self.instance.ctx.channel.id): self})
+    key: "SessionKey"
+    instance: "Menu"
+    owner: int
 
     def __repr__(self):
-        return f'Session({self.instance})'
+        return f"Session({self.instance})"
 
     def kill(self):
-        """Removes a session object from the sessions store & sets it as inactive."""
-        try:
-            del sessions[(self.owner, self.instance.ctx.channel.id)]
-            self.instance.active = False
-        except KeyError:
-            return
+        """Removes a session object from the sessions store."""
+        del sessions[(self.owner, self.instance.ctx.channel.id)]
 
     @staticmethod
-    def from_context(ctx: Context) -> "Session":
-        """Gets an existing session object from the sessions store."""
-        return sessions.get((ctx.author.id, ctx.channel.id))
+    def get(ctx: Context) -> "Session":
+        """Returns an existing session object from the sessions store."""
+        return sessions.get((ctx.author.id, ctx.channel.id), None)
+
+    @classmethod
+    def create(cls, instance: "Menu") -> "Session":
+        """Creates a new session based from a menu instance and adds it to the session store. Checks for
+        existing sessions in the store and handles safe deletion."""
+        session = Session.get(instance.ctx)
+        if session and session.key in sessions.keys():
+            if PREVENT_MULTISESSIONS is False:
+                session.kill()
+                await session.instance._safe_close()
+            else:
+                raise SessionError(
+                    f"Duplicate session in channel [{instance.ctx.channel.id}] for user [{instance.ctx.author.id}]."
+                )
+
+        self = Session()
+
+        self.key = (instance.author.id, instance.channel.id)
+        self.instance = instance
+        self.owner = instance.author.id
+
+        sessions.update({self.key: self})
+
+        return self

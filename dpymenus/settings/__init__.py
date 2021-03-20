4cc0ffc0514e33e4ac408f47688a1b1534e3bb22
Rob <rob@robwagner.dev>
2021-03-19 21:46:05 -0400

    Updated settings to use pyproject.toml

--- dpymenus/settings/__init__.py
+++ dpymenus/settings/__init__.py
@@ -1,14 +1,21 @@
-# 0 is no limit / disabled
-HISTORY_CACHE_LIMIT = 10
-SESSION_PER_CHANNEL_LIMIT = 1
-SESSION_PER_GUILD_LIMIT = 3
-SESSION_PER_USER_LIMIT = 10
-SESSION_TIMEOUT = 3600
+import os
+from pathlib import Path
+import toml
 
-ALLOW_SESSION_RESTORE = False
-HIDE_WARNINGS = False
-PREVENT_MULTISESSIONS = False
+with open(Path(os.getcwd()) / 'pyproject.toml', 'r') as file:
+    data = toml.load(file)
+    config = data.get('dpymenus', None)
 
-REPLY_AS_DEFAULT = True
+    HISTORY_CACHE_LIMIT = config.get('history-cache-limit', 10)
+    SESSION_PER_CHANNEL_LIMIT = config.get('sessions-per-channel', 1)
+    SESSION_PER_GUILD_LIMIT = config.get('sessions-per-guild', 3)
+    SESSION_PER_USER_LIMIT = config.get('sessions-per-user', 10)
+    SESSION_TIMEOUT = config.get('session-timeout', 3600)
 
-COGWATCH_INTEGRATION = False
+    ALLOW_SESSION_RESTORE = config.get('allow-session-restore', True)
+    HIDE_WARNINGS = config.get('hide-warnings', False)
+    PREVENT_MULTISESSIONS = config.get('prevent-multisessions', False)
+
+    REPLY_AS_DEFAULT = config.get('reply-as-default', True)
+
+    COGWATCH_INTEGRATION = config.get('cogwatch-integration', False)

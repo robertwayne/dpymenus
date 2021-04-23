bfa7756fa726789a854535b3742c83f046daeaec
Rob <rob@robwagner.dev>
2021-04-23 12:38:44 -0400

    Update script to use `subprocess.run`.

--- scripts.py
+++ scripts.py
@@ -1,8 +1,8 @@
 # This file is used for calling scripts through the poetry CLI.
 
-from subprocess import check_call
+from subprocess import run
 
 
 def fmt():
     """Runs black on the project source directory. Uses the config defined in `pyproject.toml`."""
-    check_call(["black", "dpymenus/", "examples"])
+    run(["black", "dpymenus/", "examples"])

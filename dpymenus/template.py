3cc306d6eb206ba0750c39b7c86887f42fa8ebd7
Rob <rob@robwagner.dev>
2021-05-05 18:11:29 -0400

    Added docstrings.

--- dpymenus/template.py
+++ dpymenus/template.py
@@ -4,18 +4,29 @@ from enum import Enum
 
 
 class FieldStyle(Enum):
+    """Defines how templated fields are handled when interacting with existing embed fields.
+    IGNORE: Templated fields are ignored. Existing fields remain. Default option.
+    COMBINE: Templated fields are combined with existing fields.
+    OVERRIDE: Templated fields override any existing fields.
+    """
     IGNORE = 2
     COMBINE = 1
     OVERRIDE = 0
 
 
 class FieldSort(Enum):
+    """Defines how templated fields are sorted when using the FieldsStyle.COMBINE mode.
+    FIRST: Templated fields are added before existing fields.
+    LAST: Templated fields are added after existing fields. Default option.
+    """
     FIRST = 1
     LAST = 0
 
 
 @dataclass
 class Template:
+    """Defines a Page template. Page templates contain most of the same options as Embed objects, but they are
+    generally applied via strings or Dictionaries. See the examples on how templates should be defined."""
     title: str = None
     description: str = None
     color: str = None

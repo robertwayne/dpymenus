5029a12bb519a77ab623885bfd7843fb3a1ec84c
Rob <rob@robwagner.dev>
2021-05-08 18:25:29 -0400

    Added script for automating readthedocs dependency file generation.

--- scripts.py
+++ scripts.py
@@ -6,3 +6,9 @@ from subprocess import run
 def fmt():
     """Runs black on the project source directory. Uses the config defined in `pyproject.toml`."""
     run(['black', 'dpymenus/', 'examples'])
+
+
+def rtd():
+    """Generates a compatible requirements.txt file from our pyproject.toml dependencies. This is
+    required by readthedocs configuration."""
+    run(['poetry', 'export', '--output', 'docs/requirements.txt', '--dev'])

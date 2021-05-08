7589cad2950493f0bc0d838eac2c2646f213b559
Rob <rob@robwagner.dev>
2021-05-08 18:27:43 -0400

    Don't generate hashes on our pinned RTD file.

--- scripts.py
+++ scripts.py
@@ -11,4 +11,4 @@ def fmt():
 def rtd():
     """Generates a compatible requirements.txt file from our pyproject.toml dependencies. This is
     required by readthedocs configuration."""
-    run(['poetry', 'export', '--output', 'docs/requirements.txt', '--dev'])
+    run(['poetry', 'export', '--output', 'docs/requirements.txt', '--dev', '--without-hashes'])

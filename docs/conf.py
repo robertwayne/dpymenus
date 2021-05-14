b6a18c63131a64f025eae5317d96a9440bce4bc1
Rob <rob@robwagner.dev>
2021-05-14 16:40:21 -0400

    Version bump.

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = '2020-2021, Rob Wagner'
 author = 'Rob Wagner'
 
 # The full version, including alpha/beta/rc tags
-release = '2.1.1'
+release = '2.1.2'
 master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
